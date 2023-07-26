3b9fafa27a052c38327167dd734f4c05110bc5a2
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-07-26 16:58:26 +0200

    row add

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ function App() {
 	return (
     <>
       <NavBar/>
-      <h1>Cosa desideri mangiare?</h1>
-      <hr/>
+      <div className="container">
+        <h1 className="text-center fw-bold fs-3 pt-2">Cosa desideri mangiare?</h1>
+        <hr/>
+        <div className="row">
+          
+        </div>
+      </div>
     </>
 	);
 }
