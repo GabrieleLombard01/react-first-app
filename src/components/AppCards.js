83bee117d7b049516c4600c5c6e221239aac7696
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-07-26 17:36:11 +0200

    style fixed

--- src/components/AppCards.js
+++ src/components/AppCards.js
@@ -3,7 +3,7 @@ import california from '../images/california.png';
 function Card() {
 	return (
         <div className="col">
-            <div className="card" style="width: 18rem;">
+            <div className="card" style={{width: '18rem', textAlign: 'center'}}>
                 <img src={california} className="card-img-top" />
                 <div className="card-body">
                     <h5 className="card-title">California Roll</h5>
